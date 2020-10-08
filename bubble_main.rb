6180011c93774710b6d8fdc020d1c4bff9116310
tahiry-dev <tahirycontact@gmail.com>
2020-10-08 17:23:48 +0300

    fix liner errors

--- bubble_main.rb
+++ bubble_main.rb
@@ -5,18 +5,12 @@ def bubble_sort(arr)
   swap = true
   while swap
     swap = false
-    # puts 'loop start'
     for i in 0...(arr.length - 1)
-      # print " i= #{i} arr[i]= #{arr[i]} arr[i+1]= #{arr[i + 1]}"
       if arr[i] > arr[i + 1]
-        # print ' swap = true '
         swap = true
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      else
-        # print ' swap = false '
+      else 
       end
-      # print arr
-      # print "\n"
     end
   end
   print 'Sorted Array -> '
@@ -24,28 +18,20 @@ def bubble_sort(arr)
 end
 
 test = [1, 100, 45, 4, 7, 0, 6, 6]
-
 puts bubble_sort(test).inspect
-
 def bubble_sort_by(arr)
   print "\n"
   puts 'Bubble Sort By'
   swap = true
   while swap
     swap = false
-    # puts 'loop start'
     for i in 0...(arr.length - 1)
-      # print " i= #{i} arr[i]= #{arr[i]} arr[i+1]= #{arr[i + 1]}"
       yield(arr[i], arr[i + 1])
       if $compare == 1
-        # print ' swap = true '
         swap = true
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
       else
-        # print ' swap = false '
       end
-      # print arr
-      # print "\n"
     end
   end
   new_arr = arr
@@ -59,3 +45,4 @@ bubble_sort_by([1, 100, 45, 4, 7, 0, 6, 6]) do |left, right|
   $compare = 0 if left == right
   $compare = -1 if left < right
 end
+
